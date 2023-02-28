3832e9434dd4b67d1193c9b41498901828569170
Evgeniy <andrej.burakov.91@mail.ru>
2023-02-28 23:59:40 +0300

    добавиил решене

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿string[] table = new string[5] {"bud", "fill", "array", "yes", "42"};
+string[] tale3 = new string[table.Length];
+
+void Fill(string[] table, string[] tale3)
+{ 
+int count = 0;
+for (int i = 0; i < table.Length; i++)
+{
+     if(table[i].Length <= 3)
+     {
+        tale3[count] = table[i];
+        count++;
+     }
+}
+}
+
+void Print(string[] table)
+{ 
+for (int i = 0; i < table.Length; i++)
+{
+    System.Console.WriteLine($"{table[i]}");
+}
+}
+
+
+Fill(table, tale3);
+Print(tale3);
